baabee78f689e521bccdd859156f7245a399bc4d
jkfechter <jk.fechter@gmail.com>
2022-05-18 08:52:52 -0700

    first commit

---
+++ src/functionBased/pages/About.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { useMatch } from "react-router-dom";
+import Header from '../components/Header';
+import NavBar from '../components/NavBar';
+
+const About = (props) => {
+  
+  return (
+    <>
+    <NavBar />
+    <Header />
+      <div>About</div>
+    <p>Description:
+    In this app, you can add, delete, submit and edit items. To edit items, simply double click on it. Once you are done, press the enter key to resubmit. This app will persist your data in the browser local storage. So whether you reload, close your app or reopened it, you still have access to your to-dos items.
+    </p>
+    </>
+  )
+}
+
+export default About
